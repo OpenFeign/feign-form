ebc473f693dbd974c7e8dffab63d26f6faa9f10a
bilak <bilaak@gmail.com>
2017-02-24 18:54:56 +0100

    - changed implementation to support Spring's RequestPart

--- feign-form-spring/src/main/java/feign/form/spring/SpringMultipartEncodedDataProcessor.java
+++ feign-form-spring/src/main/java/feign/form/spring/SpringMultipartEncodedDataProcessor.java
@@ -15,13 +15,21 @@
  */
 package feign.form.spring;
 
-import feign.codec.EncodeException;
-import feign.form.MultipartEncodedDataProcessor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.List;
+
+import feign.codec.EncodeException;
+import feign.form.MultipartEncodedDataProcessor;
 
 /**
  * Adds support for {@link MultipartFile} type to {@link MultipartEncodedDataProcessor}.
@@ -31,43 +39,108 @@ import java.io.PrintWriter;
  */
 public class SpringMultipartEncodedDataProcessor extends MultipartEncodedDataProcessor {
 
-	@Override
-	protected boolean isPayload(Object value) {
-		return super.isPayload(value) || isMultipart(value);
-	}
+  private static final List<HttpMessageConverter<?>>
+      converters =
+      new RestTemplate().getMessageConverters();
+  private static final HttpHeaders jsonHeaders = new HttpHeaders();
 
-	private boolean isMultipart(Object value) {
-		if (value instanceof MultipartFile)
-			return true;
-		if (value.getClass().isArray())
-			return isMultipartArray(value);
-		// TODO add collections/map also?
-		return false;
-	}
+  static {
+    jsonHeaders.setContentType(MediaType.APPLICATION_JSON);
+  }
 
-	private boolean isMultipartArray(Object value) {
-		return value instanceof MultipartFile[];
-	}
+  @Override
+  protected boolean isPayload(Object value) {
+    return super.isPayload(value) || isMultipart(value);
+  }
 
-	@Override
-	protected void writeByteOrFile(OutputStream output, PrintWriter writer, String name, Object value) {
-		if (isMultipart(value)) {
-			MultipartFile[] multipartFiles;
-			if (!isMultipartArray(value)) { // TODO add collections/map also?
-				multipartFiles = new MultipartFile[] { (MultipartFile) value };
-			} else {
-				multipartFiles = (MultipartFile[]) value;
-			}
-			try {
-				for (MultipartFile mpf : multipartFiles) {
-					writeByteArray(output, writer, name, mpf.getOriginalFilename(), mpf.getContentType(), mpf.getBytes());
-				}
-			} catch (IOException e) {
-				throw new EncodeException("Can't encode MultipartFile", e);
-			}
-			return;
-		}
+  private boolean isMultipart(Object value) {
+    if (value instanceof MultipartFile) {
+      return true;
+    }
+    if (value.getClass().isArray()) {
+      return isMultipartArray(value);
+    }
+    // TODO add collections/map also?
+    return false;
+  }
 
-		super.writeByteOrFile(output, writer, name, value);
-	}
+  private boolean isMultipartArray(Object value) {
+    return value instanceof MultipartFile[];
+  }
+
+  @Override
+  protected void writeByteOrFile(OutputStream output, PrintWriter writer, String name,
+                                 Object value) {
+    if (isMultipart(value)) {
+      MultipartFile[] multipartFiles;
+      if (!isMultipartArray(value)) { // TODO add collections/map also?
+        multipartFiles = new MultipartFile[]{(MultipartFile) value};
+      } else {
+        multipartFiles = (MultipartFile[]) value;
+      }
+      try {
+        for (MultipartFile mpf : multipartFiles) {
+          writeByteArray(output, writer, name, mpf.getOriginalFilename(), mpf.getContentType(),
+              mpf.getBytes());
+        }
+      } catch (IOException e) {
+        throw new EncodeException("Can't encode MultipartFile", e);
+      }
+      return;
+    }
+
+    super.writeByteOrFile(output, writer, name, value);
+  }
+
+  @Override
+  protected void writeParameter(PrintWriter writer, String name, Object value) {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    HttpOutputMessage dummyRequest = new HttpOutputMessageImpl(outputStream, jsonHeaders);
+    Class<?> requestType = value.getClass();
+    MediaType requestContentType = MediaType.APPLICATION_JSON_UTF8;
+    for (HttpMessageConverter<?> messageConverter : converters) {
+      if (messageConverter.canWrite(requestType, requestContentType)) {
+        try {
+          ((HttpMessageConverter<Object>) messageConverter)
+              .write(value, requestContentType, dummyRequest);
+          writer.append("Content-Disposition: form-data; name=\"" + name + "\"").append(CRLF);
+          writer.append("Content-Type: application/json; charset=UTF-8").append(CRLF);
+          String test = ((ByteArrayOutputStream) dummyRequest.getBody()).toString("UTF-8");
+          writer.append(CRLF).append(test);
+          return;
+        } catch (IOException e) {
+
+        }
+        return;
+      }
+    }
+    super.writeParameter(writer, name, value);
+  }
+
+  /**
+   * Minimal implementation of {@link org.springframework.http.HttpOutputMessage}. It's needed to
+   * provide the request body output stream to
+   * {@link org.springframework.http.converter.HttpMessageConverter}s
+   */
+  private class HttpOutputMessageImpl implements HttpOutputMessage {
+
+    private final OutputStream body;
+    private final HttpHeaders headers;
+
+    public HttpOutputMessageImpl(OutputStream body, HttpHeaders headers) {
+      this.body = body;
+      this.headers = headers;
+    }
+
+    @Override
+    public OutputStream getBody() throws IOException {
+      return body;
+    }
+
+    @Override
+    public HttpHeaders getHeaders() {
+      return headers;
+    }
+
+  }
 }
